f67dd0870362e9a45e2fa970ae02fe4354b32eff
Kudae Sithu <kudaesithu2@gmail.com>
2024-03-03 17:51:45 +0630

    Added complete feature

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -1,38 +1,89 @@
 <template>
-    <div class="singleProject">
-        <div>
-            <h1 class="font-bold text-green-500 pb-3  cursor-pointer "  @click="showDeatail=!showDeatail" >{{ project.title }}</h1>
-            <h3 v-if="showDeatail" class="text-md text-gray-600 font-semibold" >{{ project.detail }}</h3>
-        </div>
-        <div class="space-x-5" >
-            <i class="fa-solid fa-trash single-icon" @click="deleteProject"></i>
-            <i class="fa-solid fa-pen single-icon"></i>
-            <i class="fa-solid fa-check single-icon"></i>
-        </div>
+  <div class="singleProject" :class="{ complete: project.complete }">
+    <div>
+      <h1
+        class="font-bold text-sky-500 cursor-pointer"
+        @click="showDeatail = !showDeatail"
+      >
+        {{ project.title }}
+      </h1>
+      <h3 v-if="showDeatail" class="text-md text-gray-600 pt-3 font-semibold">
+        {{ project.detail }}
+      </h3>
     </div>
+    <div class="space-x-5">
+      <i class="fa-solid fa-trash single-icon" @click="deleteProject"></i>
+      <i class="fa-solid fa-pen single-icon"></i>
+      <i class="fa-solid fa-check single-icon" @click="completeProject"></i>
+    </div>
+  </div>
 </template>
 
 <script>
-    import axios from "axios"
-    export default {
-        props: ['project'],
-        data() {
-            return {
-                showDeatail: false,
-            }
-        },
-        methods: {
-            deleteProject() {
-                axios.delete(`http://localhost:3000/projects/${this.project.id}`).then((result) => {
-                    this.$emit('delete', this.project.id)
-                }).catch((err) => {
-                    console.log(err);
-                });
-            }
-        },
-    }
+import axios from "axios";
+export default {
+  props: ["project"],
+  data() {
+    return {
+      showDeatail: false,
+      api: "http://localhost:3000/projects/",
+    };
+  },
+  methods: {
+    deleteProject() {
+      axios
+        .delete(`http://localhost:3000/projects/${this.project.id}`)
+        .then((result) => {
+          this.$emit("delete", this.project.id);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+
+    //fetch
+    completeProject() {
+      const updateRoute = this.api + this.project.id;
+
+      const headers = {
+        "Content-Type": "application/json",
+      };
+
+      const body = JSON.stringify({ complete: !this.project.complete });
+
+      fetch(updateRoute, { method: "PATCH", headers, body })
+        .then((response) => {
+          this.$emit("complete", this.project.id);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
+
+    //axios
+    // completeProject() {
+
+    //     const updateRoute = this.api + this.project.id;
+
+    //     const headers = {
+    //         "Content-Type": "application/json",
+    //     };
+
+    //     const body = {
+    //         complete: !this.project.complete,
+    //     };
+
+    //     axios.patch(updateRoute, body, {headers})
+
+    //     .then((response) => {
+    //         this.$emit("update", this.project.id);
+    //     })
+    //     .catch((error) => {
+    //         console.error(error);
+    //     });
+    // }
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
