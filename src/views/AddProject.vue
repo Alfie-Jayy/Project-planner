1d435c7bf7abf510a93d0e3cc2d7e9dc6cc9c055
Kudae Sithu <kudaesithu2@gmail.com>
2024-03-04 10:37:00 +0630

    Added new project creation function

--- src/views/AddProject.vue
+++ src/views/AddProject.vue
@@ -1,16 +1,17 @@
 <template>
     <div class="main">
         
-        <form>
-        
+        <form @submit.prevent="AddProject" >        
                 <div>
                     <label class="label" for="">Project title</label>
-                    <input class="input" type="text">
+                    <input class="input" type="text" v-model="title" >
+                    <span v-if="titleError" class="titleError" >Project title field is required!</span>
                 </div>
 
                 <div>
                     <label class="label" for="">Project detail</label>
-                    <input class="input" type="text">
+                    <input class="input" type="text" v-model="detail" >
+                    <span v-if="detailError" class="detailError">Project detail field is required!</span>
                 </div>
 
                 <div class="text-center">
@@ -23,7 +24,55 @@
 </template>
 
 <script>
+import axios from 'axios'
     export default {
         
+        name: 'AddProject',
+        
+        data() {
+            return {
+                title: "",
+                detail: "",
+                titleError: false,
+                detailError: false
+            }
+        },
+        
+        methods: {
+            AddProject() {
+            
+                if (this.title === "") {
+                    this.titleError = true
+                } else {
+                    this.titleError = false
+                }
+
+                if (this.detail === "") {
+                    this.detailError = true
+                } else {
+                    this.detailError = false
+                }
+
+                if(this.title && this.detail){
+                    
+                    axios({
+                        method:'post',
+                        url: "http://localhost:3000/projects",
+                        data: {
+                            "title" : this.title,
+                            "detail": this.detail,
+                            "complete": false
+                        }
+                    }).then(() => {
+                        //redirect
+                        this.$router.push('/')
+                    }).catch((err) => {
+                        console.log(err);
+                    });
+                
+                }
+            
+            }
+        },
     }
 </script>
